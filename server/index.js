b8c3d7c64139cabbb68b47d2e8ce417d62ee5fdb
louhab <louhab.abderazzak@gmail.com>
2024-04-04 20:34:04 +0000

    the last update for the last commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 10;
+const db = require("./models");
+db.sequelize.sync().then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });    
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+})
 
+// nodejsreact is the name of data base to connecte 
