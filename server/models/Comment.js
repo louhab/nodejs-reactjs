f045eaad36503eeddc9ac373fe5985fea7d1a9ae
louhab <louhab.abderazzak@gmail.com>
2024-04-05 23:57:08 +0000

    adding comment partes

---
+++ server/models/Comment.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comments = sequelize.define("Comments", {
+        CommentBody: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return Comments;
+};
